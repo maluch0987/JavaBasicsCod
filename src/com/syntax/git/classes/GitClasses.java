dbd1caf642874f7451cbb8a1aabf0aae61945e57
malush0987 <qa0150783@gmail.com>
2022-06-01 21:20:54 -0400

    added a git classes

---
+++ src/com/syntax/git/classes/GitClasses.java
@@ -0,0 +1,10 @@
+package com.syntax.git.classes;
+
+public class GitClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("popculture");
+	}
+
+}
